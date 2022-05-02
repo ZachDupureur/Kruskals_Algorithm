d35a778502dc502a07b7c126f7b5bc2836873d33
Zach Dupureur <dupurezt4463@student.laccd.edu>
2022-05-02 12:05:00 -0700

    Initial commit

---
+++ header.h
@@ -0,0 +1,22 @@
+//
+// Created by zach on 5/2/22.
+//
+
+#ifndef UNTITLED1_HEADER_H
+#define UNTITLED1_HEADER_H
+
+#include "iostream"
+#include "iomanip"
+#include "vector"
+
+class Tree {
+public:
+    std::vector<char> vertex;
+    void addEdge(char vertex[]);
+};
+
+void Tree::addEdge(char *vertex) {
+
+}
+
+#endif //UNTITLED1_HEADER_H
---
+++ implementation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zach on 5/2/22.
+//
+
+#include "header.h"
